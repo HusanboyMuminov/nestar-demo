f6f50c777b2951b38700a2ba8117413c1bc09321
Husanboymuminov <xusanboymuminov2161@gmail.com>
2026-05-03 16:04:33 +0900

    feat: meta tags integrations on document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT - PAGES ROUTER")
+  //console.log("DOCUMENT - PAGES ROUTER")
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
