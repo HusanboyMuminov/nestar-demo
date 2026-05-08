58cc45767f9335009885a79132551426c1d59647
Husanboymuminov <xusanboymuminov2161@gmail.com>
2026-05-08 20:54:12 +0900

    fix: changed errors in some files

---
+++ libs/components/layout/LayoutHome.tsx
@@ -0,0 +1,46 @@
+import { Stack } from "@mui/material";
+import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
+import HeaderFilter from "../homepage/HeaderFilter";
+
+// @ts-ignore
+import "swiper/css";
+// @ts-ignore
+import "swiper/css/pagination";
+// @ts-ignore
+import "swiper/css/navigation";
+
+const withLayoutMain = (Component: any) => {
+  return (props: any) => {
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+
+        <Stack id="pc-wrap">
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
+
+          <Stack className={"header-main"}>
+            <Stack className={"container"}>
+              <HeaderFilter />
+            </Stack>
+          </Stack>
+
+          <Stack id="main">
+            <Component {...props} /> {/* => pages/index.tsx */}
+          </Stack>
+
+          <Stack id={"footer"}>
+            <Footer />
+          </Stack>
+        </Stack>
+      </>
+    );
+  };
+};
+
+export default withLayoutMain;
