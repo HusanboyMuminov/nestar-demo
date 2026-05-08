1aea46ace128f322f71f4cf8a5ba5957ec098693
Husanboymuminov <xusanboymuminov2161@gmail.com>
2026-05-08 17:54:31 +0900

    feat: property list publishing

--- pages/property/detail.tsx
+++
@@ -1,9 +0,0 @@
-import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
-
-const  PropertyDetail = () => {
-    //console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
-    return <Container>PROPERTY DETAIL</Container>
-};
-
-export default withLayoutBasic(PropertyDetail);
